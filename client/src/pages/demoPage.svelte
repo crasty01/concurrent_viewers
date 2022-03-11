34d06bd9342aea839b8bc1801c951dc3de459753
Karolis Breidokas <karolis@mc2.dev>
2022-03-11 23:04:37 +0200

    Overdrive DemoPage

--- client/src/pages/demoPage.svelte
+++ client/src/pages/demoPage.svelte
@@ -8,10 +8,13 @@
   const TIMER = 60 * 1000; // 3 * 1000;
   let timer = null;
   let animateTimer = null;
-  $: previousCount = 0;
-  $: currentCount = 0;
-  $: animateCount = 50;
+  let previousCount = 0;
+  let currentCount = 0;
+  let roundCount = 0;
+  let roundTarget = 0;
+  let animateCount = 50;
   let animatetarget = 80;
+
   const update_bar = async (current: number, last: number) => {
     previousCount = last;
     currentCount = current;
@@ -20,6 +23,8 @@
   onMount(async () => {
     const data = await fetchData("cdubya719");
     await update_bar(data.current.average, data.last.average);
+    roundTarget=previousCount
+    roundCount=Math.round(currentCount)+.5
     timer = setInterval(async () => {
       const data = await fetchData("cdubya719");
       await update_bar(data.current.average, data.last.average);
@@ -37,6 +42,24 @@
 
 <h1>Live count</h1>
 <Bar current={currentCount} target={previousCount} />
+
+<h1>Rounding</h1>
+<div>
+  <label for="config-target" >Target: </label>
+  <input id="config-target" step="0.25" type="number" bind:value={roundCount} />
+  <label for="config-count">Current count: </label>
+  <input id="config-target" type="number" bind:value={roundTarget} />
+</div>
+
+<h3>with exact values:</h3>
+<Bar current={roundCount} target={roundTarget} />
+<h3>rounded</h3>
+
+<Bar current={Math.round(roundCount)} target={Math.round(roundTarget)} />
+<h3> rounded up </h3>
+<Bar current={Math.ceil(roundCount)} target={Math.ceil(roundTarget)} />
+<h3> rounded down </h3>
+<Bar current={Math.floor(roundCount)} target={Math.floor(roundTarget)} />
 <h1>Static examples of how counter would in diferent states</h1>
 
 <Bar current={50} target={80} />
