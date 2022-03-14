3fb12f45e98a4e024663cb1638c584036c240f9e
Karolis Breidokas <karolis@mc2.dev>
2022-03-14 19:46:18 +0200

    Add target attribtute to WeekBucket
    This also adds 2 new endpoints to fetch current weeks metrics
    (/:channel/week-current) and get specific week data by timestamp

--- server/src/lib/util.ts
+++ server/src/lib/util.ts
@@ -1,26 +1,15 @@
-
 import dayjs, { Dayjs } from "dayjs";
 import isoweek from "dayjs/plugin/isoWeek";
 
 dayjs.extend(isoweek);
 
-
-
 export interface weekBucket {
-    sum: number;
-    count: number;
+  sum: number;
+  count: number;
+  target: number;
 }
-
-
 
 export function getIsoYearWeek(d: Dayjs): number {
-    const t = dayjs(d);
-    return t.isoWeekYear() * 100 + t.isoWeek();
-}
-
-export function toPayload(wb: weekBucket) {
-    return {
-        ...wb,
-        average: wb.sum / wb.count,
-    };
+  const t = dayjs(d);
+  return t.isoWeekYear() * 100 + t.isoWeek();
 }
